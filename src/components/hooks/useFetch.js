3f02d047280460913609fa95c0060b9e2667dadd
Luka B <lukab95.dev@gmail.com>
2025-10-27 03:13:37 +0100

    useFetch gotov

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -1,87 +1,61 @@
-import axios from "axios";
 import { useState, useEffect } from "react"
 
-function useFetch(url) {
+const useFetch = (url) => {
 
+    const [items, setItems] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    
+    //! fetch - nonAsync - nonDestructuring
+    // function fetchData(url) {
+    //     setLoading(true)
+    //     setError(false)
 
-const [items, setItems] = useState(null)    
-const [loading, setLoading] = useState(true)
-const [error, setError] = useState(false)
+    //     fetch(url)
+    //     .then(res=> res.json())
+    //     .then(data=> {
+    //         console.log(data);
+    //         setItems(data)
+    //     })
+    //     .catch(err=> {
+    //         setError(err)
+    //     })
+    //     .finally(()=> setLoading(false))
+    // }
 
+    //! fetch - async - nonDestructuring
+   async function fetchData(url) {
 
-
-// const fetchData = async () => {
-//     setLoading(true)
-//     setError(false)
-//     try {
-//         const {data} = await axios(url)     // const response = await axios(url) const data = data.response
-//         // const response = await fetch(url)
-//         // const data = await response.json()
-
-//         setItems(data)
-//     } catch (error) {
-//         setError(true)
-//         console.error(error)
-//     } finally {
-//         setLoading(false)
-//     }
-// }
-
-//    function fetchDataTwo (url) {
-
-//        setLoading(true)
-//        setError(false)
-
-//        axios(url)
-//        .then(({data})=> setItems(data))
-//        .catch(err=> {
-//         setError(true)
-//         console.log(err)
-//        })
-//        .finally(()=> setLoading(false))
-//     }
-
-
-    function fetchDataThree (url) {
         setLoading(true)
         setError(false)
 
-        axios(url)
-        .then(res => {
-            console.log(res.data);
-            setItems(res.data)
-        })
-        .catch(err=> {
-            console.error(err)
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data);
+            
+        } catch (err) {
+            console.error(err);
             setError(true)
-        })
-        .finally(()=> setLoading(false))
+            
+        } finally {
+            setLoading(false)
+        }
     }
- 
 
 
 
-
-useEffect(()=> {
-    if (!url){
-        console.warn("Fetch aborted: URL not provided")
-        return;
-    }
-    fetchDataThree(url)
-}, [url])
+    useEffect(()=> {
+        if (!url) {
+            console.warn("Forgot the url?");
+            return;
+        }
+        fetchData(url)
+    }, [url])
 
 
     return {items, loading, error}
 
 }
 
-
-
 export default useFetch
-
-/* component file
-import useFetch from "...destination"
-
-const url = "...api"
-const {items, loading, error} = useFetch(url)
-*/
